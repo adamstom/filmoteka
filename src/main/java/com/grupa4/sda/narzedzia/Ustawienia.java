e639701807f62f119ae070d002436a540c1746bd
NRS <normas@wp.pl>
2018-08-26 09:01:21 +0200

    Wprowadzony "Root" ścieżki filmu aby można było pracować na różnych maszynach.

--- src/main/java/com/grupa4/sda/narzedzia/Ustawienia.java
+++ src/main/java/com/grupa4/sda/narzedzia/Ustawienia.java
@@ -2,8 +2,14 @@ package com.grupa4.sda.narzedzia;
 
 
 public class Ustawienia {
-    final public static String PLIK_Z_LISTA_KLIENTOW="C:\\Users\\adams\\IdeaProjects\\mavenfilmotekaproject\\src\\main\\java\\com\\grupa4\\sda\\bazaDanych\\klient.txt";
-    final public static String PLIK_Z_LISTA_FILMOW="C:\\Users\\adams\\IdeaProjects\\mavenfilmotekaproject\\src\\main\\java\\com\\grupa4\\sda\\bazaDanych\\film.txt";
-    final public static String PLIK_Z_UNIKALNE_ID_MAX="C:\\Users\\adams\\IdeaProjects\\mavenfilmotekaproject\\src\\main\\java\\com\\grupa4\\sda\\bazaDanych\\unikalneIdMax.txt";
+
+   // final public static String SCIEZKA_ROOT="D:\\Programy\\Git\\Programowanie\\filmoteka\\src\\main\\java\\com\\grupa4\\sda\\bazaDanych\\";
+    final public static String SCIEZKA_ROOT="C:\\Users\\adams\\IdeaProjects\\mavenfilmotekaproject\\src\\main\\java\\com\\grupa4\\sda\\bazaDanych\\";
+
+    final public static String PLIK_Z_LISTA_KLIENTOW=SCIEZKA_ROOT+"klient.txt";
+    final public static String PLIK_Z_LISTA_FILMOW=SCIEZKA_ROOT+"film.txt";
+    final public static String PLIK_Z_UNIKALNE_ID_MAX=SCIEZKA_ROOT+"unikalneIdMax.txt";
+
+
 
 }
